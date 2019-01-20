1940ad3b004d6ad2ada2f5ca8d984b0bd0a945a9
Mårten Pettersson <mtnptrsn@gmail.com>
2019-01-20 09:37:10 +0100

    fix: export getParam

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import getParams from './getParams'
+import getParam from './getParam'
 import getProtocol from './getProtocol'
 import stringifyParams from './stringifyParams'
 import changeParams from './changeParams'
 
-export { getParams, getProtocol, stringifyParams, changeParams }
+export { getParams, getProtocol, stringifyParams, changeParams, getParam }
